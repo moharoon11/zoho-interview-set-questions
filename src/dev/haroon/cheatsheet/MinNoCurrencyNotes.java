2d296886dd01c42d4260559df690e65d8a06de0f
moharoon11 <moharoon11107@gmail.com>
2024-05-13 15:15:53 +0530

    may13

---
+++ src/dev/haroon/cheatsheet/MinNoCurrencyNotes.java
@@ -0,0 +1,36 @@
+package dev.haroon.cheatsheet;
+
+public class MinNoCurrencyNotes {
+
+    public static void main(String[] args) {
+        calculate(3001);
+    }
+
+    public static void calculate(int amount) {
+
+        if(amount <= 0) return;
+
+        int[] notes = {2000, 500, 200, 100, 50, 20, 10, 5, 1};
+        int[] noteCounter = new int[notes.length];
+
+        for(int i=0; i<notes.length; i++) {
+
+            // approach 1
+//            while(amount >= notes[i]) {
+//                noteCounter[i]++;
+//                amount -= notes[i];
+//            }
+
+            // approach 2
+            if(amount >= notes[i]) {
+                noteCounter[i] = amount / notes[i];
+                 amount = amount % notes[i]; /** (or) amount -= notes[i] * noteCounter[i]*/
+            }
+        }
+
+        for(int i=0; i<noteCounter.length; i++) {
+            if(noteCounter[i] != 0)
+              System.out.println("Currency = " + notes[i] + " * " + noteCounter[i]);
+        }
+    }
+}
