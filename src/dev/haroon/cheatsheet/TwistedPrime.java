aefa851474c27ecd7f3fdf2669f2bd70216ac4f3
moharoon11 <moharoon11107@gmail.com>
2024-05-15 17:50:03 +0530

    may15

---
+++ src/dev/haroon/cheatsheet/TwistedPrime.java
@@ -0,0 +1,22 @@
+package dev.haroon.cheatsheet;
+
+public class TwistedPrime {
+
+    public static void main(String[] args) {
+
+        System.out.println(isTwistedPrime(97));
+        System.out.println(isTwistedPrime(43));
+    }
+
+    public static int isTwistedPrime(int num) {
+        boolean isNumPrime = false;
+        boolean isNumTwistedPrime = false;
+
+        isNumPrime = PrintNPrimeNumbers.isPrime(num);
+        int reverseNum = Integer.parseInt(new StringBuffer("" + num).reverse().toString());
+        isNumTwistedPrime = PrintNPrimeNumbers.isPrime(reverseNum);
+
+        return isNumPrime == isNumTwistedPrime ? 1 : 0;
+    }
+
+}
