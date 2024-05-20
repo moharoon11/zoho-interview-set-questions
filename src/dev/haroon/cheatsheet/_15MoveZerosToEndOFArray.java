54306f32b579d1813938ba6dc0e7a702e800eb10
moharoon11 <moharoon11107@gmail.com>
2024-05-20 18:04:51 +0530

    may20

--- src/dev/haroon/cheatsheet/_15MoveZerosToEndOFArray.java
+++ src/dev/haroon/cheatsheet/_15MoveZerosToEndOFArray.java
@@ -5,8 +5,7 @@ import java.util.Arrays;
 public class _15MoveZerosToEndOFArray {
 
     public static void main(String[] args) {
-
-            System.out.println(Arrays.toString(moveZeros(new int[] {1,2,0,0,3,4,0})));
+             System.out.println(Arrays.toString(moveZeros(new int[] {1,2,0,0,3,4,0})));
              int[] arr1 = new int[]{1,3,0,0,4,4,0};
              moveZerosInplace(arr1);
              System.out.println(Arrays.toString(arr1));
@@ -20,11 +19,11 @@ public class _15MoveZerosToEndOFArray {
         int k = newArr.length - 1;
 
         while(i < arr.length) {
-            if(arr[i] != 0) {
+            if(arr[i] != 0)
                 newArr[j++] = arr[i++];
-            } else {
+             else
                 newArr[k--] = arr[i++];
-            }
+
         }
         return newArr;
     }
@@ -35,14 +34,11 @@ public class _15MoveZerosToEndOFArray {
         int nonZeroIndex = 0;
 
         for(int num:arr) {
-            if(num != 0) {
+            if(num != 0)
                 arr[nonZeroIndex++] = num;
-            }
         }
-
-        while(nonZeroIndex < arr.length) {
+        while(nonZeroIndex < arr.length)
             arr[nonZeroIndex++] = 0;
-        }
 
     }
 
---
+++ src/dev/haroon/cheatsheet/_17SingleNumberII.java
@@ -0,0 +1,47 @@
+package dev.haroon.cheatsheet;
+
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+
+public class _17SingleNumberII {
+
+    public static void main(String[] args) {
+        int[] nums = {0, 1, 0, 1, 0, 1, 99};
+
+
+        System.out.println(singleNumber(new int[]{0,0,0,1,1,1,10,99, 99}));
+        System.out.println(singleNumber(new int[] {1,1}));
+        System.out.println(singleNumberHashMap(new int[]{0,0,0,1,1,1,10,99, 99}));
+        System.out.println(singleNumberHashMap(new int[] {1,1}));
+    }
+
+    public static int singleNumberHashMap(int[] nums) {
+        Map<Integer, Integer> map = new HashMap<>();
+
+        for(int num:nums) {
+            map.put(num, map.getOrDefault(num, 0) + 1);
+        }
+
+        for(int num:nums) {
+            if(map.get(num) == 1) return num;
+        }
+        return -1;
+    }
+
+    public static int singleNumber(int[] nums) {
+        Arrays.sort(nums);
+
+        if(nums.length == 1 || nums[0] != nums[1]) {
+            return  nums[0];
+        }
+
+        for(int i=1; i<nums.length - 1; i++) {
+            if(nums[i] != nums[i-1] && nums[i] != nums[i+1]) return nums[i];
+        }
+
+        if(nums[nums.length - 1] != nums[nums.length -2]) return nums[nums.length-1];
+
+        return -1;
+    }
+}
